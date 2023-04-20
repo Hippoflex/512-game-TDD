c33c92e93e4dcba7945bbbff608be19c00adc232
Piryazev Yury <ypiryazev@gmail.com>
2023-04-20 20:52:47 +0700

    added tests and code to move left code in the game

--- logics.py
+++ logics.py
@@ -21,3 +21,18 @@ def get_index_from_number(num):
     num -= 1
     x, y = num // 4, num % 4
     return x, y
+
+
+def to_the_left(mas):
+    for row in mas:
+        while 0 in row:
+            row.remove(0)
+        while (len(row)) != 4:
+            row.append(0)
+        for i in range(4):
+            for j in range(3):
+                if mas[i][j] == mas[i][j + 1] and mas[i][j] != 0:
+                    mas[i][j] *= 2
+                    mas[i].pop(j + 1)
+                    mas[i].append(0)
+    return mas
--- tests.py
+++ tests.py
@@ -51,3 +51,37 @@ class Test_512(unittest.TestCase):
 
     def test_get_index_from_number_3(self):
         self.assertEqual(get_index_from_number(5), (1, 0))
+
+    def test_move_left_1(self):
+        mas = [
+            [8, 8, 0, 0],
+            [2, 0, 2, 0],
+            [256, 256, 0, 0],
+            [4, 0, 0, 4],
+        ]
+
+        rez_mas = [
+            [16, 0, 0, 0],
+            [4, 0, 0, 0],
+            [512, 0, 0, 0],
+            [8, 0, 0, 0],
+        ]
+
+        self.assertEqual(to_the_left(mas), rez_mas)
+
+    def test_move_left_2(self):
+        mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        rez_mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        self.assertEqual(to_the_left(mas), rez_mas)
